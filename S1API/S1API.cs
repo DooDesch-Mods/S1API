3c1e8448e8e68c0e907949efe2df3eb4ac6e933b
ifBars <tristens5465@gmail.com>
2026-07-24 06:04:44 -0700

    chore(release): bump version to 3.1.0-beta.6

--- S1API/S1API.cs
+++ S1API/S1API.cs
@@ -9,7 +9,7 @@ using S1API.Internal.Products;
 using S1API.Lifecycle;
 using S1API.Map;
 
-[assembly: MelonInfo(typeof(S1API.S1API), "S1API (Forked by Bars)", "3.1.0-beta.5", "KaBooMa")]
+[assembly: MelonInfo(typeof(S1API.S1API), "S1API (Forked by Bars)", "3.1.0-beta.6", "KaBooMa")]
 [assembly: MelonPriority(Int32.MinValue)]
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 namespace S1API
